91532c277a04aedaa22b5323f3fa9a0b63d8d92d
ekrabismi <ekrabismi@gmail.com>
2015-10-15 09:35:18 +0600

    15_10_2015

---
+++ include files.js
@@ -0,0 +1,10 @@
+function INCLUDE(filename)
+{
+    var head = document.getElementsByTagName('head')[0];
+
+    var script = document.createElement('script');
+    script.src = filename;
+    script.type = 'text/javascript';
+
+    head.appendChild(script)
+}
